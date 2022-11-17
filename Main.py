60c2206fcbe9e986c1d92f64e8fc5371fb3cd5ec
rahul balakavi <rahulbmv@gmail.com>
2022-11-16 22:18:06 -0500

    Add ability to run generated SQL on sqlite + remove un-needed columns from csv

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import os
 import pandas
 import streamlit as st
+import sqlite3 as sq
 from model import GeneralModel
 
 
@@ -8,7 +9,8 @@ def app():
     # Creating an object of prediction service
     pred = GeneralModel()
 
-    api_key = st.sidebar.text_input("APIkey", type="password")
+    # api_key = st.sidebar.text_input("APIkey", type="password")
+    api_key = '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'
 
     # Using the streamlit cache
     def process_prompt(table_name, question, comma_sep_col_names, values):
@@ -24,26 +26,21 @@ def app():
 
         # st.write("---")
 
-        table_name = st.text_area(
-            "Enter table name",
-            value="crimes",
-            max_chars=50,
-            height=20,
-        )
+        # table_name = st.text_area(
+        #     "Enter table name",
+        #     value="crimes",
+        #     max_chars=50,
+        #     height=20,
+        # )
+        #
 
-        comma_sep_col_names = st.text_area(
-            "Enter comma separated column names to represent table schema",
-            value="Identifier,Case Number,Crime Date,Block,Illinois Uniform Crime Reporting Code,Primary Type,"
-                  "Description,Location Description,Arrest,Domestic,Beat,District,Ward,Community Area,FBI Code,"
-                  "X Coordinate,Y Coordinate,Crime Year,Updated On,Latitude,Longitude,Location",
-            max_chars=150,
-            height=50,
-        )
-
-        uploaded_file = st.file_uploader("Or Choose a CSV file. This will overwrite the above properties")
+        uploaded_file = st.file_uploader("Choose CSV file")
         values_str = ""
+        col_names = []
+        comma_sep_col_names = ""
         if uploaded_file is not None:
             table_name = os.path.splitext(uploaded_file.name)[0]
+            table_name = table_name + "_ts"
             file_type = os.path.splitext(uploaded_file.name)[1]
             print(file_type)
             if '.csv' != file_type:
@@ -52,15 +49,29 @@ def app():
             print(table_name)
             full_file_df = pandas.read_csv(uploaded_file, warn_bad_lines=True, error_bad_lines=False)
             comma_sep_col_names = ','.join(list(full_file_df.columns.values))
+            col_names = full_file_df.columns.values;
             print(comma_sep_col_names)
             num_lines = len(full_file_df.index)
             print(num_lines)
+            conn = sq.connect('{}.sqlite'.format(table_name))  # creates file
+            full_file_df.to_sql(table_name, conn, if_exists='replace', index=False)  # writes to file
+            conn.close()
+
             df = full_file_df.head()
             for value_arr in df.values:
                 values_str += '#values(' + ','.join(map(str, value_arr)) + ')\n'
             print(df.values)
             print("values = %s", values_str)
             st.write(df)
+
+        comma_sep_col_names_filtered = comma_sep_col_names
+
+        if comma_sep_col_names:
+            col_names_filtered = st.multiselect(
+                'Select only needed column names',
+                col_names,
+                col_names)
+            comma_sep_col_names_filtered = ','.join(col_names_filtered)
 
         question = st.text_area(
             "Enter question to translate to SQL",
@@ -71,7 +82,13 @@ def app():
 
         if st.button("Submit"):
             with st.spinner(text="In progress"):
-                report_text = process_prompt(table_name, question, comma_sep_col_names, values=values_str)
-                st.markdown(report_text)
+                report_text = process_prompt(table_name, question, comma_sep_col_names_filtered, values=values_str)
+                sql = report_text.split(';', 1)[0] + ";"
+                st.subheader(sql)
+                conn = sq.connect('{}.sqlite'.format(table_name))
+                df = pandas.read_sql(sql, conn)
+                conn.close()
+                st.write(df)
+
     else:
         st.error("🔑 Please enter API Key")
